4186f6e906f771829188ccfb1912d39f474f6cb6
KBMOre <kmore8150@gmail.com>
2025-07-22 08:36:14 +0530

    Test case for new line operator

--- src/stringCalculate.js
+++ src/stringCalculate.js
@@ -1,11 +1,9 @@
  function add(numbers){
     if(numbers === "")
         return 0;
-    if(numbers.includes(",")){
+    numbers = numbers.replace(/\n/g, ',')
         return numbers.split(",").reduce((acc, num)=> acc + parseInt(num), 0)
     }
-    return parseInt(numbers);
-}
 
 module.exports = { add }
  
